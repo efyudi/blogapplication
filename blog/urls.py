ff5ee8f8bfecfd312481199ac8281532799cda3f
efyudi <fakharudin99@gmail.com>
2021-10-30 00:52:08 +0500

    Added a Like and Comment patterns

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ from .views import (
     PostUpdateView,
     PostDeleteView,
     BlogHomeListAndCreate,
+    CommentCreateView,
+    like_post,
+    post_comment,
 )
 
 
@@ -14,6 +17,7 @@ urlpatterns = [
     #path('create/', PostCreateView.as_view(), name='create_post'),
     path('update/<int:pk>', PostUpdateView.as_view(), name='update_post'),
     path('delete/<int:pk>', PostDeleteView.as_view(), name='delete_post'),
-
+    path('like/<int:pk>', like_post, name="like_post"),
+    path('comment/<int:pk>', post_comment, name="post_comment"),
     path('', BlogHomeListAndCreate.as_view(), name='blog_home'),
 ]
