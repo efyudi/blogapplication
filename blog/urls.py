04ba6cc674f4183040cea584843811ebb172e4a1
efyudi <fakharudin99@gmail.com>
2021-11-05 08:32:06 +0500

    Comment Update and Delete Added

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ from .views import (
     PostUpdateView,
     PostDeleteView,
     BlogHomeListAndCreate,
-    CommentCreateView,
+    CommentDeleteView,
+    CommentUpdateView,
     like_post,
     post_comment,
 )
@@ -15,9 +16,13 @@ from .views import (
 urlpatterns = [
     #path('', BlogHomeView.as_view(), name='blog_home'),
     #path('create/', PostCreateView.as_view(), name='create_post'),
+    path('', BlogHomeListAndCreate.as_view(), name='blog_home'),
+
     path('update/<int:pk>', PostUpdateView.as_view(), name='update_post'),
     path('delete/<int:pk>', PostDeleteView.as_view(), name='delete_post'),
     path('like/<int:pk>', like_post, name="like_post"),
+
     path('comment/<int:pk>', post_comment, name="post_comment"),
-    path('', BlogHomeListAndCreate.as_view(), name='blog_home'),
+    path('comment/delete/<int:pk>', CommentDeleteView.as_view(), name="delete_comment"),
+    path('comment/update/<int:pk>', CommentUpdateView.as_view(), name="update_comment"),
 ]
