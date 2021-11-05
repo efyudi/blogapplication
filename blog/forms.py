791f32fec69c0a74523afe697a74e135ad0bfaa7
efyudi <fakharudin99@gmail.com>
2021-11-05 08:25:38 +0500

    Manual Blog Form Added

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,25 @@
 from django import forms
-from django.forms import fields, models, widgets
-from .models import Comments
+from .models import Comments, PostModel
+
+
+class BlogForm(forms.ModelForm):
+    title = forms.CharField(max_length=200, label="Blog Title", widget=forms.TextInput(attrs={
+        "size": "50px"
+    }))
+    content = forms.CharField(label="Blog Content", widget=forms.Textarea(attrs={
+        "rows": "3",
+        "cols": "50",
+    }))
+
+    class Meta:
+        model = PostModel
+        fields = ["title", "content"]
+
 
 class CommentsForm(forms.Form):
-    body = forms.CharField(widget=forms.Textarea(attrs={
+    body = forms.CharField(label="",widget=forms.Textarea(attrs={
         "rows": "3",
+        "placeholder": "Write your comment here..."
     }))
 
     class Meta:
